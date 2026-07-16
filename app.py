17ae707e6e8a65641497595a5787de7d64c69ce2
Natarjj <natacelso2000@gmail.com>
2026-07-15 21:07:09 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+#Primeiro teste com variáveis 
+
+name = "Joca da silva"
+age = "35"
+email = "joca@gmail.com"
+birth = "10/04/1980"
+mother_frame = "Maria"
+
+print(name)
+print(age)
+print(email)
+print(birth)
+print(mother_frame)
